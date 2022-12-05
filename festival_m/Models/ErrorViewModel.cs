364eec34fb80f0b1c482b1177d741d4d20784f4f
Mike <myamauchi@my.bcit.ca>
2022-12-05 11:44:33 -0800

    Add project files.

---
+++ festival_m/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace festival_m.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
